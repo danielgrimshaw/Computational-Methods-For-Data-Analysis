ad550772499beb65aef160bfdfa760094b9fc849
Daniel Grimshaw <danielgrimshaw42@gmail.com>
2015-01-22 17:55:51 -0800

    Renamed Fourier Transforms.m

---
+++ Computational Methods for Data Analysis/Fourier_Transforms.m
@@ -0,0 +1,26 @@
+clear all; close all; clc;
+
+L=20; # Number of fundamental frequencies
+n=128;
+
+x2=linspace(-L/2,L/2,n+1); x=x2(1:n);
+
+# Frequencies
+k=(2*pi/L)*[0:n/2-1 -n/2:-1];
+
+u=sech(x); #function
+ud=-sech(x).*tanh(x); #first deriv (exact)
+u2d=sech(x)-2*sech(x).^3;#second deriv (exact)
+
+ut=fft(u); # Fourier Transform
+
+uds=ifft( (i*k).*ut ); # Approximate first deriv
+u2ds=ifft( (i*k).^2.*ut ); # Approximate second deriv
+
+ks=fftshift(k);
+# X axis is Frequencies
+#plot(x,u);
+plot(ks,abs(fftshift(ut)))
+#plot(x,ud,'r', x,uds,'mo');
+# ^^^ Plots the strength of each frequency
+
