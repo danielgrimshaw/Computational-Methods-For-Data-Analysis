37ae35ef5e9ae4f2e815872fdd994d541f468ae4
Daniel Grimshaw <danielgrimshaw42@gmail.com>
2015-02-02 21:23:52 -0800

    Made a filter
    Seems to work. Now for more testing!

--- radar.m
+++ radar.m
@@ -10,11 +10,20 @@ ks = fftshift(k);
 u = sech(t);
 ut = fft(u);
 
-noise = 20;
+noise = 10; % 20
 utn = ut+noise*(randn(1,n)+i*randn(1,n));
 un = ifft(utn);
 
-subplot(2,1,1), plot(t, u, 'k', t, abs(un), 'm'); % signal vs time
+filter = exp(-k.^2);
+utnf = filter.*utn;
+
+unf = ifft(utnf);
+
+subplot(2,1,1), plot(t, u, 'k', t, abs(un), 'm', ... % signal vs time
+    t, abs(unf), 'g', ...
+    t, 0*t+0.5, 'k:');
 subplot(2,1,2), plot(ks, abs(fftshift(ut))/max(abs(fftshift(ut))), 'k', ...
-    ks, abs(fftshift(utn))/max(abs(fftshift(utn))), 'm');
+    ks, abs(fftshift(utn))/max(abs(fftshift(utn))), 'm', ...
+    ks, fftshift(filter), 'b', ...
+    ks, abs(fftshift(utnf))/max(abs(fftshift(utnf))), 'g');
 axis([-25,25,0,1]);
